e36deab23515a9e9e4d11ce48a9e6405950412de
bnsch98 <fehsbook@gmx.de>
2024-05-24 18:06:02 +0200

    gitignore hinzugefügt, symlinks, read_aol.py

--- src/test.py
+++ src/test.py
@@ -1,1 +1,11 @@
-print("this is a test")
+import ir_datasets
+dataset = ir_datasets.load('msmarco-passage/train')
+
+# for query in dataset.queries_iter(): # Will download and extract MS-MARCO's queries.tar.gz the first time
+#     ...
+
+cnt = 0
+for query in dataset.queries_iter(): # Will download and extract MS-MARCO's queries.tar.gz the first time
+    cnt += 1
+print(cnt)
+
