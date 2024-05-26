7acc01472b24e6a76d1cd5cd1096ef85c0a1fde6
bnsch98 <fehsbook@gmx.de>
2024-05-26 17:12:04 +0200

    New literature added

--- src/test.py
+++ src/test.py
@@ -7,5 +7,10 @@ dataset = ir_datasets.load('msmarco-passage/train')
 cnt = 0
 for query in dataset.queries_iter(): # Will download and extract MS-MARCO's queries.tar.gz the first time
     cnt += 1
-print(cnt)
+print(f"size msmarco data set: {cnt}")
 
+dataset2 = ir_datasets.load("aol-ia")
+cnt = 0
+for query in dataset2.queries_iter():
+    cnt+= 1
+print(f"size aolia data set: {cnt}")
