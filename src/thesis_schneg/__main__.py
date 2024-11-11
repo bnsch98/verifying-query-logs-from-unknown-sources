a12ac7a557ed31344c26685eca6a38fedf46b81c
Jan Heinrich Merker <heinrich.merker@uni-jena.de>
2024-11-11 23:17:52 +0100

    Add simple CLI

---
+++ src/thesis_schneg/__main__.py
@@ -0,0 +1,34 @@
+from typing import Optional
+
+from cyclopts import App
+
+from thesis_schneg.classification import (
+    DatasetName,
+    PredictorName,
+    classify as _classify,
+)
+
+app = App()
+
+
+@app.command
+def classify(
+    predictor: PredictorName,
+    dataset: DatasetName,
+    sample_files: Optional[int] = None,
+    only_english: bool = False,
+    read_concurrency: Optional[int] = None,
+    predict_concurrency: Optional[int] = None,
+) -> None:
+    _classify(
+        predictor_name=predictor,
+        dataset_name=dataset,
+        sample_files=sample_files,
+        only_english=only_english,
+        read_concurrency=read_concurrency,
+        predict_concurrency=predict_concurrency,
+    )
+
+
+if __name__ == "__main__":
+    app()
